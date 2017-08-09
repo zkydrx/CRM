6cdee51f8ff84ab02e9d1962826c43ac1ad741dc
ZKYAbbot <1964483239@qq.com>
2017-08-10 00:19:13 +0800

    Initial commit

---
+++ src/com/java1234/service/impl/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.java1234.service.impl;
+
+import com.java1234.dao.UserDao;
+import com.java1234.entity.User;
+import com.java1234.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户Service实现类
+ * @author Administrator
+ *
+ */
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+	@Resource
+	private UserDao userDao;
+	
+	@Override
+	public User login(User user) {
+		return userDao.login(user);
+	}
+
+	@Override
+	public List<User> findUser(Map<String, Object> map) {
+		return userDao.findUser(map);
+	}
+
+	@Override
+	public int updateUser(User user) {
+		return userDao.updateUser(user);
+	}
+
+	@Override
+	public Long getTotalUser(Map<String, Object> map) {
+		return userDao.getTotalUser(map);
+	}
+
+	@Override
+	public int addUser(User user) {
+		return userDao.addUser(user);
+	}
+
+	@Override
+	public int deleteUser(Integer id) {
+		return userDao.deleteUser(id);
+	}
+
+}
